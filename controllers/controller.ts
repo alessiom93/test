449f24e8ee7893f30a9edd8d7e1844f2f24c7a15
Alex_93 <106242360+alessiom93@users.noreply.github.com>
2025-05-08 15:06:50 +0200

    Update controller.ts

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -8,8 +8,8 @@ const test1 = async (req: Request, res: Response) => {
   const query = req.query
   try {
     // @ts-ignore
-    console.log(req.rawHeaders["x-memori-user-email"])
-    console.log(req)
+    console.log(req.headers["x-memori-user-email"])
+    console.log(req.headers)
     console.log("body", body)
     console.log("params", params)
     console.log("query", query)

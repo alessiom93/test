ef87c2df31243540833974efe848f43314e89aff
Alex_93 <106242360+alessiom93@users.noreply.github.com>
2025-06-19 15:00:32 +0200

    Update controller.ts

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -30,7 +30,26 @@ const test1 = async (req: Request, res: Response) => {
   console.log("--- test1 done ---");
 };
 
+const test1 = async (req: Request, res: Response) => {
+  console.log("=== test2 started ===");
+  const body = req.body
+  const params = req.params
+  const query = req.query
+  try {
+    console.log("body", body)
+    console.log("params", params)
+    console.log("query", query)
+    console.log("test2 successful")
+    res.status(200).send("test2 successful")
+  } catch (error) {
+    console.error('test2 failed:', error)
+    res.status(404).send('test2 failed')
+  }
+  console.log("--- test2 done ---");
+};
+
 
 module.exports = {
-  test1
+  test1,
+  test2
 };
